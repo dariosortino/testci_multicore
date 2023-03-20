3ceea4a97c31bb461ef910f8bcfda8d5129e1164
Dario Sortino <dario.sortino@iit.it>
2023-03-20 14:55:47 +0100

    add lin 2

--- multicore/src/main.c
+++ multicore/src/main.c
@@ -11,6 +11,7 @@ int main(void)
 {
 	printk("Hello world from %s\n", CONFIG_BOARD);
 	printk("Hello world from %s\n", CONFIG_BOARD);
+	printk("Hello world from %s\n", CONFIG_BOARD);
 
 	return 0;
 }
